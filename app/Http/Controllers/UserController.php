b2cec20f44d27370ba6149baf681c524dba9ef27
mukul-arahman <mukul.arahman@gmail.com>
2020-11-27 09:46:55 +0600

    Added ActivityLog

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserCollection;
 use App\Http\Resources\User as UserResource;
+
 class UserController extends Controller
 {
     /**
